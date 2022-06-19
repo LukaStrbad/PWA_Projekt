a5e2c6ced833dc7991e1807d3b8aab52d6693623
LukaStrbad <s.luka.1308@gmail.com>
2022-06-19 18:55:59 +0200

    Change database name

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $GLOBALS["dbc"] = new mysqli('localhost', 'root', '', 'projekt');
+    $GLOBALS["dbc"] = new mysqli('localhost', 'root', '', 'pwa_projekt_luka_strbad');
 } catch (Exception $e) {
     echo '<h1>Baza podataka nije dostupna</h1>';
     exit;
--- db/projekt.sql
+++ db/projekt.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `projekt`
 --
-CREATE DATABASE IF NOT EXISTS `projekt` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `projekt`;
+CREATE DATABASE IF NOT EXISTS `pwa_projekt_luka_strbad` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pwa_projekt_luka_strbad`;
 
 -- --------------------------------------------------------
 
